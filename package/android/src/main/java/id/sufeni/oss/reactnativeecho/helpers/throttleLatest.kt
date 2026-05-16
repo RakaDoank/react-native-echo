334d9e0d9b78c6efe0862898824b15a4dd00ca93
RakaDoank <rakaaudira@gmail.com>
2026-05-17 01:36:26 +0700

    improve throttleLatest in current scope

--- package/android/src/main/java/id/sufeni/oss/reactnativeecho/helpers/throttleLatest.kt
+++ package/android/src/main/java/id/sufeni/oss/reactnativeecho/helpers/throttleLatest.kt
@@ -1,25 +1,26 @@
 package id.sufeni.oss.reactnativeecho.helpers
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-// https://stackoverflow.com/a/57252799
+// Originally from https://stackoverflow.com/a/57252799
 
 fun <T> throttleLatest(
   intervalMs: Long,
-  coroutineScope: CoroutineScope,
-  destinationFunction: (T) -> Unit
-): (T) -> Unit {
+  destinationFunction: suspend (T) -> Unit
+): suspend (T) -> Unit {
   var throttleJob: Job? = null
   var latestParam: T
   return { param: T ->
     latestParam = param
     if (throttleJob?.isCompleted != false) {
-      throttleJob = coroutineScope.launch {
-        delay(intervalMs)
-        latestParam.let(destinationFunction)
+      throttleJob = coroutineScope {
+        launch {
+          delay(intervalMs)
+          destinationFunction(latestParam)
+        }
       }
     }
   }
